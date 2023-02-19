3c04840f88bf9886abfe2fd1f065b530b98c5159
Anonsatyam <satyamdeveloper98@gmail.com>
2023-02-20 00:51:39 +0530

    Made Components and Started Working On Navbar Component

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Info from './Components/Info/Info';
+import Search from './Components/Search/Search';
+import Support from './Components/Support/Support';
+import Lounge from './Components/Lounge/Lounge';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      {/* <Home/>
+      <Search/>
+      <Support/>
+      <Info/>
+      <Lounge/> */}
+    </div>
+  )
+}
+
+export default App
